f1b51372158a9ef24e0689c1e458706c2d8d54d5
okandemirel <okn.118dmrl@gmail.com>
2022-02-27 20:26:04 +0300

    Player Manager & Controllers added

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    #region Self Variables
+
+    #region Public Variables
+    #endregion
+
+    #region Serialized Variables
+    [SerializeField] private PlayerMovementController movementController;
+    [SerializeField] private PlayerAnimationController animationController;
+    [SerializeField] private PlayerPhysicsController physicsController;
+
+    #endregion
+
+    #region Private Variables
+    #endregion
+
+    #endregion
+
+
+    public void OnPlayerCollidedWithCollectable()
+    {
+        animationController.PlayAttackAnimation();
+
+    }
+
+}
